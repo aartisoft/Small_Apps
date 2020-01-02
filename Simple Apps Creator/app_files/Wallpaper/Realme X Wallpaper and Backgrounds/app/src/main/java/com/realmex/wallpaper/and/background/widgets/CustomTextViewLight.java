878cf868be4706cc10505626859e230ac4ba0cfc
Bhautik <bhautik007700@gmail.com>
2020-01-02 18:14:29 +0530

    Added Wallpaper App

---
+++ Simple Apps Creator/app_files/Wallpaper/Realme X Wallpaper and Backgrounds/app/src/main/java/com/realmex/wallpaper/and/background/widgets/CustomTextViewLight.java
@@ -0,0 +1,34 @@
+package com.realmex.wallpaper.and.background.widgets;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+@SuppressLint("AppCompatCustomView")
+public class CustomTextViewLight extends TextView {
+
+    public CustomTextViewLight(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public CustomTextViewLight(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CustomTextViewLight(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        if (!isInEditMode()) {
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "OpenSans-Regular.ttf");
+            setTypeface(tf);
+        }
+    }
+
+}
