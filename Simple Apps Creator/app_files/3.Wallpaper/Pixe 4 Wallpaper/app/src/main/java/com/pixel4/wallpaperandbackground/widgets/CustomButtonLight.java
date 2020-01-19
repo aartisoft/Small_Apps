f9ff978d296a6715562306c28228d6f80a9a9c98
Bhautik <bhautik007700@gmail.com>
2020-01-19 11:19:15 +0530

    Wallpaper App Add

---
+++ Simple Apps Creator/app_files/3.Wallpaper/Pixe 4 Wallpaper/app/src/main/java/com/pixel4/wallpaperandbackground/widgets/CustomButtonLight.java
@@ -0,0 +1,29 @@
+package com.pixel4.wallpaperandbackground.widgets;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+public class CustomButtonLight extends androidx.appcompat.widget.AppCompatButton {
+
+    public CustomButtonLight(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public CustomButtonLight(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CustomButtonLight(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "OpenSans-Regular.ttf");
+        setTypeface(tf);
+    }
+
+}
